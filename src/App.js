656f65c7e3d8e522f83b810ae0479aab685ee166
noeun0 <61595578+noeun0@users.noreply.github.com>
2021-05-11 02:44:46 +0900

    JSX, props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ // ./는 현재 파일이 있는 폴더
 
+ function Food({fav}){
+   
+   return<h1>I love {fav}</h1>
+ }
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>hello</h1>
+      <Food fav="kimch"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
     </div>
-  );
+  );    
 }
 
 export default App;
